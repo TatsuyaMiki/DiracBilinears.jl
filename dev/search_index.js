66bcf0cd33572fd5248af1c9a12c22a246a899cf
Documenter.jl <documenter@juliadocs.github.io>
2024-12-22 11:55:21 +0000

    build based on 6b0f309

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DiracBilinears","category":"page"},{"location":"#DiracBilinears","page":"Home","title":"DiracBilinears","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DiracBilinears.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DiracBilinears]","category":"page"}]
+}
